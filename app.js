ecc005524cf64187b546829a7465997a55037525
Pavel Sheremet <pashqa.sheremet@gmail.com>
2021-09-17 21:26:42 +0300

    refactoring and add animation

--- app.js
+++ app.js
@@ -1,77 +1,55 @@
-const galleryItems = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+import galleryItems from './gallery.js';
 
-// Create and render gallery
+// get refs
 
-const galleryRef = document.querySelector('.js-gallery');
-const modalRef = document.querySelector('.js-lightbox');
-const modalImageRef = document.querySelector('.lightbox__image');
-const modalBtnRef = document.querySelector('.lightbox__button');
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.js-lightbox'),
+  modalImage: document.querySelector('.lightbox__image'),
+  modalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+  modalContent: document.querySelector('.lightbox__content'),
+};
 
-let siblingList = {};
+let currentLiRef;
+
+// Class toggle functions
+
+const toggleHidden = () => document.body.classList.toggle('is-hidden');
+const toggleNext = () => refs.modalContent.classList.toggle('next');
+const toggleNextNext = () => refs.modalContent.classList.toggle('next-next');
+
+// Scrolling animation
+
+const imageAnimation = () => {
+  const isNext = refs.modalContent.classList.contains('next');
+  if (isNext) {
+    toggleNext();
+    setTimeout(toggleNextNext, 250);
+  } else {
+    setTimeout(toggleNext, 250);
+    toggleNextNext();
+  }
+};
+
+const modalFilling = obj => {
+  refs.modalImage.src = obj.dataset.source;
+  refs.modalImage.alt = obj.alt;
+  refs.modal.classList.add('is-open');
+  toggleNext();
+};
+
+const modalClear = () => {
+  const isNext = refs.modalContent.classList.contains('next');
+
+  refs.modal.classList.remove('is-open');
+  refs.modalContent.classList.remove(isNext ? 'next' : 'next-next');
+  setTimeout(() => {
+    refs.modalImage.src = '';
+    refs.modalImage.alt = '';
+  }, 500);
+};
+
+// create and insert markup
 
 const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
@@ -91,9 +69,11 @@ const galleryMarkup = galleryItems
   })
   .join('');
 
-galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryRef.addEventListener('click', onGalleryItemClick);
+// Open modal by click on the gallery item
+
+refs.gallery.addEventListener('click', onGalleryItemClick);
 
 function onGalleryItemClick(e) {
   e.preventDefault();
@@ -102,42 +82,44 @@ function onGalleryItemClick(e) {
     return;
   }
 
-  modalImageRef.src = targetObj.dataset.source;
-  modalImageRef.alt = targetObj.alt;
-  modalRef.classList.add('is-open');
-  document.body.classList.toggle('is-hidden');
+  modalFilling(targetObj);
 
-  siblingList = e.target.closest('li');
+  toggleHidden();
+
+  currentLiRef = targetObj.closest('li');
 
   window.addEventListener('keydown', onEscPress);
   window.addEventListener('keydown', onPrevNextPress);
 }
 
-modalRef.addEventListener('click', modalClose);
+// Close modal by close button and overlay click
+
+refs.modal.addEventListener('click', modalClose);
 
 function modalClose(e) {
   const targetObj = e.target;
   if (
     !(
       targetObj.classList.contains('lightbox__overlay') ||
-      targetObj.classList.contains('lightbox__button')
+      targetObj === refs.modalBtn
     )
   ) {
     return;
   }
-  e.currentTarget.classList.remove('is-open');
-  document.body.classList.toggle('is-hidden');
-  modalImageRef.src = '';
+
+  toggleHidden();
+  modalClear();
 
   window.removeEventListener('keydown', onEscPress);
   window.removeEventListener('keydown', onPrevNextPress);
 }
 
+// Close modal by Escape
+
 function onEscPress(e) {
   if (e.code === 'Escape') {
-    modalRef.classList.remove('is-open');
-    document.body.classList.toggle('is-hidden');
-    modalImageRef.src = '';
+    toggleHidden();
+    modalClear();
 
     window.removeEventListener('keydown', onEscPress);
     window.removeEventListener('keydown', onPrevNextPress);
@@ -149,21 +131,28 @@ function onPrevNextPress(e) {
     return;
   }
 
-  if (e.code === 'ArrowRight' && siblingList.nextSibling) {
-    modalImageRef.src =
-      siblingList.nextSibling.querySelector('.gallery__image').dataset.source;
-    modalImageRef.alt =
-      siblingList.nextSibling.querySelector('.gallery__image').alt;
-    siblingList = siblingList.nextSibling;
+  if (e.code === 'ArrowRight' && currentLiRef.nextSibling) {
+    const nextImage = currentLiRef.nextSibling.querySelector('.gallery__image');
+    setTimeout(() => {
+      refs.modalImage.src = nextImage.dataset.source;
+      refs.modalImage.alt = nextImage.alt;
+    }, 125);
+
+    imageAnimation();
+
+    currentLiRef = currentLiRef.nextSibling;
   }
 
-  if (e.code === 'ArrowLeft' && siblingList.previousSibling) {
-    modalImageRef.src =
-      siblingList.previousSibling.querySelector(
-        '.gallery__image',
-      ).dataset.source;
-    modalImageRef.alt =
-      siblingList.previousSibling.querySelector('.gallery__image').alt;
-    siblingList = siblingList.previousSibling;
+  if (e.code === 'ArrowLeft' && currentLiRef.previousSibling) {
+    const prevImage =
+      currentLiRef.previousSibling.querySelector('.gallery__image');
+    setTimeout(() => {
+      refs.modalImage.src = prevImage.dataset.source;
+      refs.modalImage.alt = prevImage.alt;
+    }, 200);
+
+    imageAnimation();
+
+    currentLiRef = currentLiRef.previousSibling;
   }
 }
